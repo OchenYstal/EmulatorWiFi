4bcc2159ba6f696645c7dee0ddf3fb13b5ac3cc7
OchenYstal <ystal242@gmail.com>
2025-12-11 12:54:58 +0300

    initcommit

---
+++ build/CMakeFiles/wifi_emulator.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wifi_emulator.dir/link.d"
+  "CMakeFiles/wifi_emulator.dir/src/ipc/socket_server.cpp.o"
+  "CMakeFiles/wifi_emulator.dir/src/ipc/socket_server.cpp.o.d"
+  "CMakeFiles/wifi_emulator.dir/src/main.cpp.o"
+  "CMakeFiles/wifi_emulator.dir/src/main.cpp.o.d"
+  "wifi_emulator"
+  "wifi_emulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wifi_emulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
